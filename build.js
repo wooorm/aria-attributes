6fcfc2e60b0821835727c4fbe481e12aa8b7542e
Titus Wormer <tituswormer@gmail.com>
2017-06-25 17:09:41 +0200

    Refactor build script

--- build.js
+++ build.js
@@ -3,33 +3,31 @@
 var fs = require('fs');
 var path = require('path');
 var https = require('https');
-var cheerio = require('cheerio');
 var bail = require('bail');
+var concat = require('concat-stream');
+var unified = require('unified');
+var parse = require('rehype-parse');
+var selectAll = require('hast-util-select').selectAll;
 var list = require('./');
 
-https.get('https://www.w3.org/TR/wai-aria/complete', function (res, err) {
-  var value = '';
+https.get('https://www.w3.org/TR/wai-aria/complete', function (res) {
+  res.pipe(concat(onconcat)).on('error', bail);
 
-  bail(err);
+  function onconcat(buf) {
+    var tree = unified().use(parse).parse(buf);
 
-  res
-    .setEncoding('utf8')
-    .on('data', function (buf) {
-      value += buf;
-    })
-    .on('end', function () {
-      cheerio.load(value)('#index_state_prop dt a').each(function () {
-        var data = this.attribs.href.slice(1);
+    selectAll('#index_state_prop dt a', tree).forEach(function (node) {
+      var data = node.properties.href.slice(1);
 
-        if (data && list.indexOf(data) === -1) {
-          list.push(data);
-        }
-      });
-
-      fs.writeFile(
-        path.join(__dirname, 'index.json'),
-        JSON.stringify(list.sort(), 0, 2) + '\n',
-        bail
-      );
+      if (data && list.indexOf(data) === -1) {
+        list.push(data);
+      }
     });
+
+    fs.writeFile(
+      path.join(__dirname, 'index.json'),
+      JSON.stringify(list.sort(), 0, 2) + '\n',
+      bail
+    );
+  }
 });
