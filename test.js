777633e5e2e596bcb5bd5e396e585f9b9f413571
Titus Wormer <tituswormer@gmail.com>
2022-11-16 11:39:07 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,21 +1,17 @@
-import assert from 'node:assert'
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {ariaAttributes} from './index.js'
 
-test('ariaAttributes', function (t) {
-  t.ok(Array.isArray(ariaAttributes), 'should be an array')
+test('ariaAttributes', function () {
+  assert.ok(Array.isArray(ariaAttributes), 'should be an array')
 
-  t.doesNotThrow(function () {
-    let index = -1
+  let index = -1
 
-    while (++index < ariaAttributes.length) {
-      assert.strictEqual(
-        typeof ariaAttributes[index],
-        'string',
-        '`' + ariaAttributes[index] + '` should be a string'
-      )
-    }
-  }, 'should be an array of strings')
-
-  t.end()
+  while (++index < ariaAttributes.length) {
+    assert.strictEqual(
+      typeof ariaAttributes[index],
+      'string',
+      '`' + ariaAttributes[index] + '` should be a string'
+    )
+  }
 })
